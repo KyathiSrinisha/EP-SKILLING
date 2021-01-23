f3ca0b884f796b248755d29bc387d6944044bf33
KyathiSrinisha <74000954+KyathiSrinisha@users.noreply.github.com>
2021-01-23 18:15:07 +0530

    Add files via upload

---
+++ JDBC/src/com/klu/Bean/StudentBean.java
@@ -0,0 +1,28 @@
+package com.klu.Bean;
+
+public class StudentBean {
+	private int Regno;
+    private String Name;
+    private String Email;
+	public int getRegno() {
+		return Regno;
+	}
+	public void setRegno(int Regno) {
+		this.Regno = Regno;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String Name) {
+		this.Name = Name;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String Email) {
+		this.Email = Email;
+	}
+    
+	
+}
+
